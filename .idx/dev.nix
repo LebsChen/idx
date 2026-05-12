166c7ab1b47232969c8181af29173d899686b844
LebsChen <openclaw@openclaw-02.localhost>
2026-05-12 11:48:18 +0000

    Add ttyd web terminal preview

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -3,9 +3,18 @@
   packages = [
     pkgs.openssh
     pkgs.curl
+    pkgs.ttyd
   ];
   env = {};
   idx = {
+    previews = {
+      previews = {
+        web = {
+          command = [ "ttyd" "-p" "$PORT" "bash" ];
+          manager = "web";
+        };
+      };
+    };
     workspace = {
       onCreate = {
         setup-ssh-tunnel = "chmod +x ./start-ssh-tunnel.sh && ./start-ssh-tunnel.sh";
