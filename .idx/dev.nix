c9a1a8c13031cf3a1a863ca8097102019e484f94
LebsChen <98428162+LebsChen@users.noreply.github.com>
2026-05-14 07:25:47 +0000

    Update dev.nix

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -3,13 +3,27 @@
   packages = [
     pkgs.openssh
     pkgs.git
+    pkgs.debianutils
+    pkgs.unzip
+    pkgs.qemu_kvm
+    pkgs.sudo
+    pkgs.cdrkit
+    pkgs.cloud-utils
+    pkgs.qemu
+    pkgs.openssl
+    pkgs.curl
   ];
-  
+
+  services = {
+    docker.enable = true;
+  };
+    
   idx = {
     workspace = {
       onCreate = {
         setup-ssh = ''
           chmod +x ./start.sh
+          ./start.sh
         '';
       };
       onStart = {
